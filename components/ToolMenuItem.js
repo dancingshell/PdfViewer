8dbbfc0d97cc1846ba72309f640b95430d677d0e
Kaitlin Jaffe <kaitlin.jaffe@procore.com>
2019-04-26 10:29:38 -0700

    updating toolbars

---
+++ components/ToolMenuItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Icon } from 'expo';
+import { Platform, TouchableOpacity, Text, View } from 'react-native';
+
+import Colors from '../constants/Colors';
+
+export default function ToolMenuIcon({icon, label, focused, onClick}) {
+  return (
+    <TouchableOpacity
+      onPress={() => onClick(icon)}
+      style={{
+        flex: 1,
+        width: 50,
+        height: 50,
+      }}
+    >
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Icon.Ionicons
+          name={Platform.OS === "ios" ? `ios-${icon}` : `md-${icon}`}
+          size={26}
+          color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+        />
+        <Text style={{color: focused ? Colors.tabIconSelected : Colors.tabIconDefault}}>{label}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
--- screens/Viewer.js
+++ screens/Viewer.js
@@ -1,15 +1,73 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, Platform} from 'react-native';
+import {Svg} from 'expo';
 import { connect } from 'react-redux';
+import Toolbar from '../components/ToolBar';
 
 class Viewer extends React.Component {
+  state = {
+    selectedTab: '',
+    activeTool: 'rect'
+  }
   render() {
     return (
-      <View>
-        <Text>Test</Text>
+      <View style={{flex: 1}}>
+        <ToolbarContainer>
+          {
+            this.state.selectedTab === 'pencil' && (
+              <DrawMenu activeTool={this.state.activeTool} onToolChange={(tool) => this.setState({activeTool: tool})}/>
+            )
+          }
+          <Toolbar selectedTab={this.state.selectedTab} onSelect={(tab) => this.setState({selectedTab: tab})}/>
+        </ToolbarContainer>
       </View>
     );
   }
 }
-export default Viewer
-// export default connect()(Viewer);
+
+function ToolbarContainer({children}) {
+  return (
+    <View style={{
+      flex:1,
+      height: '100%',
+      alignItems: 'center',
+      backgroundColor: '#fbfbfb',
+      justifyContent: 'flex-end'
+    }}
+    >
+      {children}
+    </View>
+  )
+}
+
+const drawTools = [
+ 'freehand',
+  'line',
+  'arrow',
+  'rect',
+  'ellipse',
+  'cloud'
+];
+
+
+function DrawMenu({activeTool, onToolChange}) {
+  return (
+    <View style={{flexDirection: 'row'}}>
+      {
+        drawTools.map(toolName =>
+          <DrawItem toolName={toolName} isSelected={toolName === activeTool} onPress={() => onToolChange(toolName)}/>
+        )
+      }
+    </View>
+  )
+}
+
+function DrawItem({ toolName, isSelected, onPress }) {
+  return (
+    <View>
+      <Text onPress={onPress} style={{color: isSelected ? 'red': 'black'}}>{toolName}</Text>
+    </View>
+  )
+}
+
+export default connect()(Viewer);
